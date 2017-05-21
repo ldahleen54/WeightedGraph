43f665beea7f81372f4714ce02bd41d0efd6ed51
ldahleen54 <ldahleen54@gmail.com>
2017-05-20 20:45:42 -0500

    Small Changes

--- MatrixGraph.py
+++ MatrixGraph.py
@@ -1,10 +1,15 @@
+#a class for a vertex object
+#it is more consistent with the list graph format but can probably be ignored for just using a string for the vertex name
 class Vertex:
     def __init__(self, name):
         self.name = name
 
 class Graph:
+    #set of vertices added to the graph
     vertices = {}
+    #edge matrix
     edges = []
+    #indices for the edges to find it in the matrix
     edge_indices = {}
 
     def add_vertex(self, vertex):
@@ -14,6 +19,8 @@ class Graph:
                 row.append(0)
             self.edges.append([0] * (len(self.edge_indices)))
             self.edge_indices[vertex.name] = len(self.edge_indices)
+            return True
+        elif isinstance(vertex, basestring):
             return True
         else:
             return False
@@ -32,24 +39,25 @@ class Graph:
             str += edge
         for edgeIndex in self.edge_indices:
             str += self.edge_indices[edgeIndex]
+
     # Prints edges matrix
-    def print_graph(self):
+    # def print_graph(self):
         # print self.edges[0][1]
         # for edge in self.edges:
         #     print edge
         # for edgeIndex in self.edge_indices:
         #     print self.edge_indices[edgeIndex]
 
-
-g = Graph()
-a = Vertex('A')
-g.add_vertex(a)
-g.add_vertex(Vertex('B'))
-for k in range(ord('A'), ord('K')):
-    g.add_vertex(Vertex(chr(k)))
-
-
-
-
-
-g.print_graph()
+#
+# g = Graph()
+# a = Vertex('A')
+# g.add_vertex(a)
+# g.add_vertex(Vertex('B'))
+# for k in range(ord('A'), ord('K')):
+#     g.add_vertex(Vertex(chr(k)))
+#
+#
+#
+#
+#
+# g.print_graph()

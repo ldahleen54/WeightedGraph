b9925ae6006b8685a1f2e5d88e6c53de37147f8a
ldahleen54 <ldahleen54@gmail.com>
2017-06-06 16:14:10 -0500

    Small Changes

--- MatrixGraph.py
+++ MatrixGraph.py
@@ -1,8 +1,5 @@
 #a class for a vertex object
 #it is more consistent with the list graph format but can probably be ignored for just using a string for the vertex name
-class Vertex:
-    def __init__(self, name):
-        self.name = name
 
 class Graph:
     #set of vertices added to the graph
@@ -13,14 +10,25 @@ class Graph:
     edge_indices = {}
 
     def add_vertex(self, vertex):
-        if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
-            self.vertices[vertex.name] = vertex
+        # if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
+        #     self.vertices[vertex.name] = vertex
+        #     for row in self.edges:
+        #         row.append(0)
+        #     self.edges.append([0] * (len(self.edge_indices)))
+        #     self.edge_indices[vertex.name] = len(self.edge_indices)
+        #     return True
+        if len(self.vertices) == 0:
+            # self.edges.append(0)
+            for row in self.edges:
+                row.append([0])
+        if isinstance(vertex, basestring) and vertex not in self.vertices:
+            self.vertices[vertex] = vertex
+
+            self.edges.append([0] * (len(self.edge_indices)))
             for row in self.edges:
                 row.append(0)
-            self.edges.append([0] * (len(self.edge_indices)))
-            self.edge_indices[vertex.name] = len(self.edge_indices)
-            return True
-        elif isinstance(vertex, basestring):
+
+            self.edge_indices[vertex] = len(self.edge_indices)
             return True
         else:
             return False
@@ -34,30 +42,16 @@ class Graph:
             return False
 
     def __str__(self):
-        str = self.edges[0][1]
-        for edge in self.edges:
-            str += edge
-        for edgeIndex in self.edge_indices:
-            str += self.edge_indices[edgeIndex]
 
-    # Prints edges matrix
-    # def print_graph(self):
-        # print self.edges[0][1]
-        # for edge in self.edges:
-        #     print edge
-        # for edgeIndex in self.edge_indices:
-        #     print self.edge_indices[edgeIndex]
 
-#
-# g = Graph()
-# a = Vertex('A')
-# g.add_vertex(a)
-# g.add_vertex(Vertex('B'))
-# for k in range(ord('A'), ord('K')):
-#     g.add_vertex(Vertex(chr(k)))
-#
-#
-#
-#
-#
-# g.print_graph()
+        string = ''
+        string = self.edge_indices.values().__str__()
+
+        string2 = '\n'
+        for edgelist in self.edges:
+            for edge in edgelist:
+                string2 += edge.__str__()
+            string2 += '\n'
+        return string + string2
+
+
